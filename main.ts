7abe1bbb57e5aed5fd4c0130ef487bf65a1e06ed
fukusenta <42712029+fukusenta@users.noreply.github.com>
2023-12-27 16:18:10 +0900

    テスト用にshowTemperature関数を作ってみた

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
-})
+namespace Hoge {
+    //% color=190 weight=100 icon="\f840" block="Basic Blocks"
+    export function showTemperature(temp: number): void {
+        basic.showString(temp.toString() + "!");
+    }
+}
+
+// basic.forever(function() {
+//     Hoge.showTemperature(5);
+// })
