07a3f261b00e4f0614b47488e31577152dcbdc8b
fukusenta <42712029+fukusenta@users.noreply.github.com>
2023-12-27 16:50:08 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 //% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
 namespace Hoge {
     //% blockId=hoge_show_temperature
-    //% block="show temperature $temp"
+    //% block="show|temperature $temp"
     export function showTemperature(temp: number): void {
         basic.showString(temp.toString() + "!");
     }
 }
 
-// basic.forever(function() {
+// basic.forever(function () {
 //     Hoge.showTemperature(5);
 // })
